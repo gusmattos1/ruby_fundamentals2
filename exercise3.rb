38aae45aefadfda067bc70917dbd30bc514d3b02
gusmattos1 <gusmattos@gmail.com>
2018-03-20 16:35:37 -0400

    exercise 3 done

---
+++ exercise3.rb
@@ -0,0 +1,19 @@
+puts "\nExercise 3 \n\n"
+
+def is_even(number)
+  if number % 2 ==0
+    return true
+  else
+    return false
+  end
+end
+
+puts is_even(1)
+puts is_even(2)
+puts is_even(3)
+puts is_even(4)
+puts is_even(5)
+puts is_even(6)
+puts is_even(7)
+puts is_even(8)
+puts is_even(9)
