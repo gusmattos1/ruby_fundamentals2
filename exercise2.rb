3f3f4222aa2c2be262e0e78800f59d3990a85d9d
gusmattos1 <gusmattos@gmail.com>
2018-03-20 16:26:16 -0400

    exercise 2 done

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+p "exercise 2"
+
+def negative(number)
+  if number >0
+    return true
+  else
+    return false
+  end
+end
+
+puts negative(-1)
+puts negative(1)
+puts negative(-2)
+puts negative(2)
+puts negative(-3)
+puts negative(3)
+puts negative(-4)
+puts negative(4)
+puts negative(-5)
